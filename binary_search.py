a4b9d35c6feb8bbecf80d2f798e935fe50398f02
fong-a <fong.a@saintaug.nsw.edu.au>
2022-07-02 08:47:12 +1000

    Updated to be function

--- binary_search.py
+++ binary_search.py
@@ -1,23 +1,57 @@
-# BINARY SEARCH
-search_array = [1,2,3,9,11,13,17,25,57,90]
-search_term = int(input("Please enter your search criteria")) #99
+# # BINARY SEARCH
 
-low = 0
-high = len(search_array)-1
-mid = (high + low)//2 #integer division - cuts off the decimal
-found = False # flag
+# search_array = [1,2,3,9,11,13,17,25,57,90]
+# search_term = int(input("Please enter your search criteria")) #99
 
-while found == False and low < high:
-    mid = (high + low) //2
-    if search_term == search_array[mid]:
-        found = True
-    else:
-        if search_term > search_array[mid]:
+# low = 0
+# high = len(search_array)-1
+# mid = (high + low)//2 #integer division - cuts off the decimal
+# found = False # flag
+
+# while found == False and low < high:
+#     mid = (high + low) //2
+#     if search_term == search_array[mid]:
+#         found = True
+#     else:
+#         if search_term > search_array[mid]:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+            
+# if found == True:
+#     print("Found")
+# else:
+#     print("Number not found")
+    
+# Updated v1.0
+
+# Binary Search in python
+
+def binarySearch(array, x, low, high):
+
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] < x:
             low = mid + 1
+
         else:
             high = mid - 1
-            
-if found == True:
-    print("Found")
+
+    return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
 else:
-    print("Number not found")
+    print("Not found")
